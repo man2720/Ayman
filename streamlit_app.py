187f7948f604f1c2fe59f73589afc6e5f716574b
man2720 <aymanzabero@gmail.com>
2025-05-26 21:54:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.write(
     "Ini adalah website yang di buat untuk melengkapi tugas infomatika Ayman Zaber Oemar."
 )
 st.write(
-    "ini adalah foto foto kenangan di kelas X-I"
+    "mbe"
 )
 st.image(
     "IMG-20250505-WA0000.jpg",width=200
