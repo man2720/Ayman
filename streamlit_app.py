bf5e589c6fdcc4bd3488b3186a9583190c7a2855
man2720 <aymanzabero@gmail.com>
2025-05-27 09:18:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,8 @@
 import streamlit as st
 
 st.title("--selamat datang--")
-st.write(
-    "Ini adalah website yang di buat untuk melengkapi tugas infomatika Ayman Zaber Oemar."
-)
-st.write(
-    "mbe"
-)
-st.image(
-    "IMG-20250505-WA0000.jpg",width=200
-)
-st.write(
-    "kurban tentar lagi euyy"
-)
- st.write(
-     "eid Mubarak"
-)
+st.write("Ini adalah website yang di buat untuk melengkapi tugas infomatika Ayman Zaber Oemar.")
+st.write("mbe")
+st.image("IMG-20250505-WA0000.jpg",width=200)
+st.write("kurban tentar lagi euyy")
+st.write("eid Mubarak")
